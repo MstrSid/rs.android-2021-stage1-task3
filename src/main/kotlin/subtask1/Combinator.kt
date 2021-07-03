6966140d66790a04cdfd04a052dc2936de91ff57
Eugene Kos <mstrsid@live.com>
2021-07-04 01:09:48 +0300

    done

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -2,8 +2,22 @@ package subtask1
 
 class Combinator {
 
-    // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        fun factorial(n : Int) : Int {
+            var factorial = 1
+            for (i in 1..n) {
+                factorial *= i
+            }
+            return factorial
+        }
+        for(n in 1..array[1]){
+            if(array[0]*factorial(n)*factorial((array[1]-n)) == factorial(array[1])) {
+                return n
+            }
+            else {
+                continue
+            }
+        }
+        return null
     }
 }
--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -4,6 +4,40 @@ class SquareDecomposer {
 
     // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        var temp=number*number
+        var sq=0
+        val resultList = mutableListOf<Int>()
+        var element = number-1
+        do {
+            resultList.add(element)
+            temp -=element*element
+            if(temp==0)
+                return resultList.reversed().toTypedArray()
+            if(element==1)
+            {
+                if (resultList.size < 2)
+                {
+                    break
+                }
+                temp += resultList.last() * resultList.last()
+                resultList.removeAt(resultList.lastIndex)
+                element = resultList.last() - 1
+                temp += resultList.last() * resultList.last()
+                resultList.removeAt(resultList.lastIndex)
+            }
+            else
+            {
+                sq = kotlin.math.sqrt(temp.toFloat()).toInt()
+                if(element<=sq) {
+                    element--
+                }
+                else {
+                    element = sq
+                }
+            }
+        }
+        while (element>0)
+
+        return null
     }
 }
